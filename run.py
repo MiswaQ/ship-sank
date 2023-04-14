ed0e2c19a7b4ff77446282e066c0504d1808cc77
MiswaQ <>
2023-04-14 19:43:24 +0000

    Added more row and range to board and added hit and miss syntacs

--- run.py
+++ run.py
@@ -5,14 +5,14 @@ def start_game():
     This will be the first page user sees
     where the player choses name. And starts the game.
     '''
-    possibility = 88
+    possibility = 100
     ships = 5
     print(' <>'*14)
     print(' <>         Welcome to Ship Sank!       <>')
     print(' <>  Sink the opponents ship!           <>')
     print(f' <>  Board Size: {possibility}                     <>')
     print(f' <>  Num of ships: {ships}                    <>')
-    print(' <>  Chose between numbers: 00 to 88!   <>')
+    print(' <>  Chose between numbers: 00 to 99!   <>')
     print(' <>  One point for a hit!               <>')
     print(f' <>  First to {ships} wins!                   <>')
     print(' <>'*14)
@@ -26,18 +26,38 @@ def start_game():
     print(f'Welcome to the game! {player_name}!\n')
 
     def player_board():
-        print('  Sink_Sank_Ship!')
-        print(f'      {player_name}')
-        print('  0 1 2 3 4 5 6 7')
-        for x in range(8):
-            print(x, '@ '*8)
+        print('     Sink_Sank_Ship!')
+        print(f'         {player_name}')
+        print('   0  1  2  3  4  5  6  7  8  9')
+
+       
+        # To be able to change the symbols when hit and miss # we will add one more loop!
+        # To know where we hit we will add place and hit/miss
+        place = 0
+        hit = [22]
+        miss = [21]
+        for x in range(10):
+            row = ''
+            for y in range(10):
+                symb = ' @ '
+                if place in hit:
+                    symb = ' H '
+                elif place in miss:
+                    symb = ' M '
+                row = row + symb
+                place = place + 1
+            print(x,row)
     player_board()
 
     def computer_board():
-        print('      Computer')
-        print('  0 1 2 3 4 5 6 7')
-        for x in range(8):
-            print(x, '@ '*8)
+        print('\n         Computer')
+        print('   0  1  2  3  4  5  6  7  8  9')
+        for x in range(10):
+            row = ''
+            for y in range(10):
+                symb = ' @ '
+                row = row + symb
+            print(x,row)
     computer_board()
 
 
