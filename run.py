fb2a9c14f19c7aa5daada21a80f03c28d2836383
MiswaQ <>
2023-04-14 16:25:22 +0000

    Changed places to at for wave sign

--- run.py
+++ run.py
@@ -6,14 +6,14 @@ def player_board():
     print('      Player')
     print('  0 1 2 3 4 5 6 7')
     for x in range(8):
-        print(x, '_ '*8)
+        print(x, '@ '*8)
     print('\n')
 
 def computer_board():
     print('      Computer')
     print('  0 1 2 3 4 5 6 7')
     for x in range(8):
-        print(x, '_ '*8)
+        print(x, '@ '*8)
     print('\n')
 
 
